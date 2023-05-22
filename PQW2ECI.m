40f6412d02b2d8b0c353d00dfc4606715069e5fa
gh731 <gh731@GENESIS>
2023-05-22 23:16:32 +0900

    PQW2ECI

---
+++ PQW2ECI.m
@@ -0,0 +1,5 @@
+output out = PQW2ECI(arg_prg,inc_angle,RAAN,A)
+R(RAAN,3'')=[cos(RAAN) -sin(RAAN) 0; sin(RAAN) cos(RAAN) 0; 0 0 1] ;
+R(inc_angle,1')=[1 0 0;0 cos(inc_angle) -sin(inc_angle);0 sin(inc_angle) cos(inc_angle)];
+R(arg_prg,3)=[1 0 0;0 cos(arg_prg) -sin(arg_prg);0 sin(arg_prg) cos(arg_prg)];
+out = R(RAAN,3'') * R(inc_angle,1') * R(arg_prg,3) *A ;
