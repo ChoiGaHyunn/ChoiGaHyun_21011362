193e1ffb735b5d238216b6cae5ca43f908cf5dd7
gh731 <gh731@GENESIS>
2023-06-07 06:43:10 +0900

    DCM

---
+++ DCM_X.m
@@ -0,0 +1,4 @@
+function MT_X = DCM_X(a)
+MT_X = [1 0 0; 0 cosd(a) -sind(a); 0 sind(a) cosd(a)];
+end
+
---
+++ DCM_Z.m
@@ -0,0 +1,3 @@
+function MT_Z = DCM_Z(c)
+MT_Z = [cosd(c) -sind(c) 0; sind(c) cos(c) 0;0 0 1];
+end
