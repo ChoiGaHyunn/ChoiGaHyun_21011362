7314119eded0c34c025c8e72366b962763659f3d
gh731 <gh731@GENESIS>
2023-06-07 05:50:46 +0900

    range in perifocal frame

--- solveRangeInPerifocalFrame.m
+++ solveRangeInPerifocalFrame.m
@@ -1,10 +1,6 @@
 function rangeInPQW = solveRangeInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly);
 mu = 3.986004418 * 10^14;
-prompt1 = "What is the semimajor_axis? ";
-prompt2 = "What is the eccentricity? ";
-prompt3 = "What is the true_anomaly? ";
-semimajor_axis = input(prompt1);
-eccentricity = input(prompt2);
-true_anomaly = input(prompt3);
-rangeInPQW = [semimajor_axis(1)*cos(true_anomaly) semimajor_axis(2)*sin(true_anomaly) 0];
+perigee = semimajor_axis*(1-eccentricity^2);
+r = perigee/(1+eccentricity*cosd(true_anomaly));
+rangeInPQW = r.*[cosd(true_anomaly); sind(true_anomaly);0];
 end
