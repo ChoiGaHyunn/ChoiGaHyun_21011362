61b1d44438ea11e22b06e1d11ceef666087ec301
gh731 <gh731@GENESIS>
2023-06-07 05:44:43 +0900

    velocity in perifocalframe

--- solveVelocityInPerifocalFrame.m
+++ solveVelocityInPerifocalFrame.m
@@ -1,11 +1,5 @@
 function velocityInPQW = solveVelocityInPerifocalFrame(semimajor_axis, eccentricity, true_anomaly);
-mu = 3.986004418 * 10^14;
-prompt1 = "What is the semimajor_axis? ";
-prompt2 = "What is the eccentricity? ";
-prompt3 = "What is the true_anomaly? ";
-semimajor_axis = input(prompt1);
-eccentricity = input(prompt2);
-true_anomaly = input(prompt3);
-perigee = 1;
-velocityInPQW = sqrt(mu/perigee)*[-sin(true_anomaly) eccentricity+cos(true_anomaly) 0];
+mu = 3.986004418 * 10^5; % [km^3/sec^2]
+perigee = semimajor_axis(1-eccentricity^2);
+velocityInPQW = sqrt(mu/perigee)*[-sin(true_anomaly); eccentricity+cos(true_anomaly) 0];
 end
