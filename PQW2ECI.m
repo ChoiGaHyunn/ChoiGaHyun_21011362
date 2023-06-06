1b6aa993d40b407e83261634337e99406b60e282
gh731 <gh731@GENESIS>
2023-06-07 06:03:16 +0900

    peri to eci

--- PQW2ECI.m
+++ PQW2ECI.m
@@ -1,6 +1,6 @@
-output out = PQW2ECI(arg_prg,inc_angle,RAAN,A)
-R(RAAN,3'')=[cos(RAAN) -sin(RAAN) 0; sin(RAAN) cos(RAAN) 0; 0 0 1] ;
-R(inc_angle,1')=[1 0 0;0 cos(inc_angle) -sin(inc_angle);0 sin(inc_angle) cos(inc_angle)];
-R(arg_prg,3)=[1 0 0;0 cos(arg_prg) -sin(arg_prg);0 sin(arg_prg) cos(arg_prg)];
-out_1 = R(RAAN,3'') * R(inc_angle,1') * R(arg_prg,3) *A ;
-out = transpose(out_1);
+function out = PQW2ECI(arg_prg,inc_angle,RAAN,A)
+R(RAAN,3'')=[cosd(RAAN) -sind(RAAN) 0; sind(RAAN) cosd(RAAN) 0; 0 0 1] ;
+R(inc_angle,1')=[1 0 0;0 cosd(inc_angle) -sind(inc_angle);0 sind(inc_angle) cosd(inc_angle)];
+R(arg_prg,3)=[1 0 0;0 cosd(arg_prg) -sind(arg_prg);0 sind(arg_prg) cosd(arg_prg)];
+out = transpose(R(RAAN,3'') * R(inc_angle,1') * R(arg_prg,3)) *A ;
+end
